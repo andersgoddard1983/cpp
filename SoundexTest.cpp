005f14ca72115bf59bc4aff0a1d3e67c98b4826a
andersgoddard1983 <anders@campaignforalcoholfree.com>
2020-06-06 07:46:39 +0000

    Working through Jeff Langr's Soundex example. 2.1 through 2.13 complete.

--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -1,6 +1,53 @@
 #include "gmock/gmock.h"    //<label id="code.include"/>
+#include "Soundex.h"
 
-TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) { //<label id="code.test"/>
-   Soundex soundex;   //<label id="code.construct"/>
+using namespace testing;
+
+class SoundexEncoding: public Test{
+public:
+  Soundex soundex;
+};
+
+TEST_F(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) { //<label id="code.test"/>
+
+   auto encoded = soundex.encode("A");
+
+   ASSERT_THAT(encoded, Eq("A000"));
+
 }
 
+TEST_F(SoundexEncoding, PadsWithZerosToEnsureThreeDigits){
+
+  auto encoded = soundex.encode("I");
+
+  ASSERT_THAT(encoded, Eq("I000"));
+
+}
+
+TEST_F(SoundexEncoding, ReplacesConsonantsWithAppropriateDigits){
+  ASSERT_THAT(soundex.encode("Ax"), Eq("A200"));
+}
+
+TEST_F(SoundexEncoding, IgnoresNonAlphabetics) {
+  ASSERT_THAT(soundex.encode("A#"), Eq("A000"));
+}
+    
+TEST_F(SoundexEncoding, ReplacesMultipleConsonantsWithDigits) {
+  ASSERT_THAT(soundex.encode("Acdl"), Eq("A234"));
+}
+
+TEST_F(SoundexEncoding, LimitsLengthToFourCharacters) {
+  ASSERT_THAT(soundex.encode("Dcdlb").length(), Eq(4u));
+}
+
+TEST_F(SoundexEncoding, IgnoresVowelLikeLetters) {
+  ASSERT_THAT(soundex.encode("Baeiouhycdl"), Eq("B234"));
+}
+
+TEST_F(SoundexEncoding, CombinesDuplicateEncodings) {
+  ASSERT_THAT(soundex.encodedDigit('b'), Eq(soundex.encodedDigit('f')));
+  ASSERT_THAT(soundex.encodedDigit('c'), Eq(soundex.encodedDigit('g')));
+  ASSERT_THAT(soundex.encodedDigit('d'), Eq(soundex.encodedDigit('t')));
+
+  ASSERT_THAT(soundex.encode("Abfcgdt"), Eq("A123"));
+}
